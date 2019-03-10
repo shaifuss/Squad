d136c109c47232254b94b791c7d7bcfa69e59e07
shaifuss <47380627+shaifuss@users.noreply.github.com>
2019-03-10 21:13:53 +0200

    saving some progress

--- decode.py
+++ decode.py
@@ -1,7 +1,43 @@
 import json
-import sklearn
+import nltk
+import numpy as np
 
-def decode ():
+
+def tokenize(sentences):
+    """
+    Extracts all words from sentences, returns as set
+    :param sentences:
+    :return: words: sorted set of all words in sentences, in list form
+    """
+    words = []
+    for sentence in sentences:
+        w = nltk.corpus.word_extraction(sentence)
+        words.extend(w)
+    clean_words = [w.lower() for w in words if w not in nltk.corpus.stopwords.words('english')]
+    clean_words = sorted(list(set(clean_words)))
+    return words
+
+
+def generate(sentences):
+    """
+    generates overall vocabulary and builds wordcount vectors for each sentence
+    :param sentences:
+    :return: matrix of word count vectors (one per sentence)
+    """
+    vocab = tokenize(sentences)
+    mat = np.zeros(len(sentences), len(vocab))
+    for index, sentence in enumerate(sentences):
+        words = nltk.corpus.word_extraction(sentence)
+        bag_vector = np.zeros(len(vocab))
+        for w in words:
+            for i, word in enumerate(vocab):
+                if word == w:
+                    bag_vector[i] += 1
+        mat[:, index] = bag_vector
+    return mat
+
+
+def unpack():
     with open('C:\\Users\\Harriet\\PycharmProjects\\Squad\\dev-v2.0.json') as jfile:
         dataset = json.load(jfile)
         context = []
@@ -18,8 +54,9 @@ def decode ():
             for j in range(len(temp)):
                 q_list.append(temp[j]['question'])
 
-    vectorizer = sklearn.feature_extraction.text.CountVectorizer()
-    p_vectors = vectorizer.fit_transform(context)
-    blink = 182
+
+
+
+
 
 
--- driver.py
+++ driver.py
@@ -1,4 +1,4 @@
 import decode
 
 if __name__ == '__main__':
-    decode.decode()
+    decode.unpack()
