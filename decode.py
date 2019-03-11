47dc00a81f2ad1ca4158a90569d46e7073d2f9b2
shaifuss <47380627+shaifuss@users.noreply.github.com>
2019-03-11 10:37:44 +0200

    saving some progress

--- decode.py
+++ decode.py
@@ -1,22 +1,26 @@
 import json
 
 
-def unpack():
+def unpack(filename):
+    """
+    unpacks dev-v2 json file and loads data into lists
+    :param filename:
+    :return: list of paragraphs, list of questions
+    """
+    #
     with open('C:\\Users\\Harriet\\PycharmProjects\\Squad\\dev-v2.0.json') as jfile:
         dataset = json.load(jfile)
         context = []
         qas = []
-        for i in range(len(dataset['data'])):   # for each article
-            temp = dataset['data'][i]['paragraphs']
-            for j in range(len(temp)):          # for each paragraph in given article
-                context.append((temp[j]['context']))
-                qas.append((temp[j]['qas']))
-        # filter out questions
+        for article in dataset['data']:   # for each article
+            for paragraph in article['paragraphs']:          # for each paragraph in given article
+                context.append(paragraph['context'])
+                qas.append(paragraph['qas'])
+        # separate each question
         q_list = []
-        for i in range(len(qas)):
-            temp = qas[i]
-            for j in range(len(temp)):
-                q_list.append(temp[j]['question'])
+        for qa in qas:
+            for q in qa:
+                q_list.append(q['question'])
 
     return context, q_list
 
--- driver.py
+++ driver.py
@@ -1,12 +1,25 @@
+"""
+
+"""
+
+
 import decode
-import bag_of_words
+import sys
+import bag_of_words as bow
 
 
-MAX_PARAGRAPHS = 100
+MAX_PARAGRAPHS = 5
 MAX_QS = 1
 
 
 if __name__ == '__main__':
-    context, q_list = decode.unpack()
-    mat = bag_of_words.generate(context[0:MAX_PARAGRAPHS], q_list[0:MAX_QS])
+    if len(sys.argv) != 2 or sys.argv[1] != 'dev-v2.0.json':
+        raise Exception('Usage: driver dev-v2.0.json')
+
+    # create lists of paragraphs and questions
+    context, q_list = decode.unpack(sys.argv[1])
+    # retrieve index of most like paragraph
+    index = bow.find_paragraph(bow.generate(context[0:MAX_PARAGRAPHS], q_list[0:MAX_QS]))
+    print('The answer most likely can be found in the following paragraph', context[index], sep="\n")
+
 
